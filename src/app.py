417d9ae19b4c4b9314245a216f50117313c22be7
jaspxr <70887995+jaspxr@users.noreply.github.com>
2025-12-22 17:35:22 +0100

    calling data module and adding config

---
+++ src/app.py
@@ -0,0 +1,13 @@
+from config import Config
+from data import load_stock_data, select_close_series
+
+def main():
+    config = Config()
+
+    # Fetch and prepare close prices
+    stock_data = load_stock_data(config.ticker, config.start_date, config.interval)
+    close = select_close_series(stock_data, config.ticker)
+    close.name = "price"
+if __name__ == "__main__":
+    main()
+
